73f5a27b1e3fce94b43e62b043fc1eccd57ec729
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-01-30 13:17:14 -0300

    fix: melhoras no código

--- Resolucoes/OperacoesDicionario.cs
+++ Resolucoes/OperacoesDicionario.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Resolucoes
 {
     public static class OperacoesDicionario
@@ -11,20 +6,15 @@ namespace Resolucoes
         public static Dictionary<string, int> CountWords(string text)
         {
             Dictionary<string, int> values = new Dictionary<string, int>();
-            string[] sep = new string[] { " ", ", ", "; ", "! ", "? ", ",", ".", ";",  "!", "?"};
-
-            List<string> words = text.Split(sep, StringSplitOptions.RemoveEmptyEntries).ToList();
+            char[] sep = [' ', ',', ';', '!', '?', ',', '.', ':', '-'];
+            var words = text.Split(sep, StringSplitOptions.RemoveEmptyEntries).ToList();
 
             foreach (var w in words)
             {
                 if(values.TryGetValue(w.ToLower(), out int count))
-                {
                     values[w] = ++count;
-                }
                 else
-                {
                     values.Add(w.ToLower(), 1);
-                }
             }
 
             return values;
